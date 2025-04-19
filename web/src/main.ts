a828b952375832df4c21211651b57836e406882e
ridintek <rw056h@gmail.com>
2025-04-20 02:00:58 +0700

    Add vue router

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,5 +1,30 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: () => import('./Home.vue')
+  }
+]
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+router.beforeEach((to) => {
+  console.log(to);
+
+  if (to.name !== 'home') {
+    return { name: 'home' }
+  }
+
+  return true
+})
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
