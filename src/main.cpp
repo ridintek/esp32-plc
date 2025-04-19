bdb4267655184f9c3754b1c6df01646e36c4a300
ridintek <rw056h@gmail.com>
2025-04-20 01:19:32 +0700

    Add introduction in serial

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define VERSION "1.0.0"
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial.print(F("RIDINTEK PLC v"));
+  Serial.println(VERSION);
+  Serial.println(F("Copyright (C) 2025 Ridintek Industri"));
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  // Main loop
 }
